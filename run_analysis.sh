4c7be22d65b29a774b14ee97d2737f8699f7a4ee
LauriHermas <lauri.hermas@gmail.com>
2026-04-08 18:56:54 +0300

    adding analysis script

---
+++ run_analysis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=project_2018026
+#SBATCH --job-name=analysisjobi
+#SBATCH --output=log_file.txt
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --partition=small
+
+module load python-data
+
+python analysis.py
+
